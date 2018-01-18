1e0e74e1026bc9a34762a21881cdc90bf65dbf43
Diep Nguyen Van <diep.nguyen@adslot.com>
2018-01-18 17:42:01 +0700

    code

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import App from './App';
 import HomeAppReducers from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-let store = createStore(HomeAppReducers);
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import logger from './middlwares/logger';
+import crashReporter from './middlwares/crashReporter';
+
+let store = createStore(
+  HomeAppReducers,
+  applyMiddleware(logger, crashReporter)
+);
 
 var provider = (
   <Provider store={store}>
